6765ad298b48226e09923a2816461d4ee6b695f1
AveryOcean <averytoad65@gmail.com>
2022-09-21 15:58:16 +0200

    Merge code for loading profile into one function and add functionality to custom flags (just takes you to the CDN image link if you click on them)

--- Viewer/Viewer/Profile.cs
+++ Viewer/Viewer/Profile.cs
@@ -99,6 +99,9 @@ namespace Viewer
 
         const string customFlagTooltip = "This is a custom flag set by the owner of this profile.";
         const string nonCustomFlagTooltip = "This is a LGBTQ flag. To see more about it double-click on this item. (Internet required!)";
+
+        const string imageCDN = "https://dclu0bpcdglik.cloudfront.net/images/";
+        const string flagEnd = "-flag.png";
         WebClient client;
 
         GlobalProfile globalProfile = new GlobalProfile();
@@ -136,147 +139,15 @@ namespace Viewer
             return copy;
         }
 
-        public bool FetchUser(string jsonPath, Image pfp = null)
+        private bool FetchUserBase(Image pfp)
         {
-            //Disable save profile button
-            saveProfile.Hide();
-
-            //Let JsonConvert do the dirty work of converting to the Profile Struct for us.
-            try
-            {
-                string json = File.ReadAllText(jsonPath);
-                globalProfile = JsonConvert.DeserializeObject<GlobalProfile>(json);
-            } catch
-            {
-                MessageBox.Show("User data invalid!");
-                return false;
-            }
-
             //-- USERNAME --
             //Get Username from global profile and put it into the username label
             username.Text = globalProfile.username;
 
-            //-- FORM TITLE --
-            //Set form title with username (e.g.: A user with the username of "jai_" would get: "jai_ - pronouns.page (LOCAL SAVE)" for the form title.
-            Text = $"{globalProfile.username} - pronouns.page (LOCAL SAVE)";
-
             //-- PROFILE PICTURE --
-            if(pfp != null)
+            if (pfp != null)
                 profilePicture.Image = pfp;
-
-            //-- PRIMARY LANGUAGE PROFILE --
-            //Get the profile with the primary language of the user (usually English)
-            try
-            {
-                primaryProfile = globalProfile.profiles.First().Value;
-            }
-            catch
-            {
-                MessageBox.Show("User not found!");
-                return false;
-            }
-
-            //-- AGE --
-            //Get age from language profile, convert to string and insert into age label
-            age.Text = primaryProfile.age.ToString();
-
-            //-- NAMES --
-            //Get list of names, serialize into proper formatting with ratings
-            var serializedNames = LanguageProfile.SerializeDictionary(primaryProfile.names);
-
-            //Convert names to a list and insert into list view
-            var namesToList = LanguageProfile.StringToListItems(serializedNames);
-            namesList.Items.Clear();
-            namesList.Items.AddRange(namesToList);
-
-            //Insert names into label for names
-            //names.Text = serializedNames;
-
-            //-- PRONOUNS --
-            //Parse any potential noun pronouns that could exist like "kit/kit's" (example from @jai_)
-            var parsedPronouns = ParseNounPronouns(primaryProfile.pronouns);
-
-            //Parse pronouns and rating into string
-            var serializedPronouns = LanguageProfile.SerializeDictionary(parsedPronouns);
-
-            //Convert pronouns to a list and insert into list view
-            var pronounsToList = LanguageProfile.StringToListItems(serializedPronouns);
-            pronounsList.Items.Clear();
-            pronounsList.Items.AddRange(pronounsToList);
-
-            //Display parsed stirng in pronouns textbox
-            //pronouns.Text = serializedPronouns;
-
-            //-- DESCRIPTION --
-            description.Text = primaryProfile.description;
-
-            //-- Links --
-            var linkList = LanguageProfile.StringToListItems(primaryProfile.links);
-            links.Clear();
-            links.Items.AddRange(linkList);
-
-            //-- Flags --
-            var flagList = LanguageProfile.StringToListItems(primaryProfile.flags, nonCustomFlagTooltip);
-            var customFlagStringList = LanguageProfile.GetAllItems(primaryProfile.customFlags);
-            var customFlagList = LanguageProfile.StringToListItems(customFlagStringList, customFlagTooltip);
-            flags.Clear();
-            flags.Items.AddRange(flagList);
-            flags.Items.AddRange(customFlagList);
-
-            //-- Words --
-            if (primaryProfile.words == null) return true;
-
-            //Compile words from all sub-categories into one huge list
-            string compiledWords = "";
-            foreach(var dict in primaryProfile.words)
-            {
-                compiledWords += LanguageProfile.SerializeDictionary(dict);
-            }
-
-            //Convert the words to a list and insert into list view
-            var wordsToList = LanguageProfile.StringToListItems(compiledWords);
-            wordsList.Items.Clear();
-            wordsList.Items.AddRange(wordsToList);
-
-            return true;
-        }
-
-        public bool FetchUser(string user)
-        {
-            if (string.IsNullOrEmpty(user))
-            {
-                MessageBox.Show("Please input a username!");
-                return false;
-            }
-
-            //-- SETUP --
-            string userFetchFull = userFetch + user;
-
-            //-- JSON GETTING & PARSING --
-            //Get JSON from pronouns.page API
-            string json = client.DownloadString(userFetchFull);
-
-            //Let JsonConvert do the dirty work of converting to the Profile Struct for us.
-            globalProfile = JsonConvert.DeserializeObject<GlobalProfile>(json);
-
-            //-- USERNAME --
-            //Get Username from global profile and put it into the username label
-            username.Text = globalProfile.username;
-
-            //-- FORM TITLE --
-            //Set form title with username (e.g.: A user with the username of "jai_" would get: "jai_ - pronouns.page" for the form title.
-            Text = $"{globalProfile.username} - pronouns.page";
-
-            //-- PROFILE PICTURE --
-            try
-            {
-                //Download profile picture from data
-                var pfp = client.DownloadData(globalProfile.avatar);
-
-                //Parse byte data to the image and insert into picture box
-                profilePicture.Image = ImageUtils.ToImage(pfp);
-            }
-            catch { /* Profile Picture doesn't exist. Just keep default. */ }
 
             //-- PRIMARY LANGUAGE PROFILE --
             //Get the profile with the primary language of the user (usually English)
@@ -355,6 +226,70 @@ namespace Viewer
             return true;
         }
 
+        public bool FetchUser(string user)
+        {
+            if (string.IsNullOrEmpty(user))
+            {
+                MessageBox.Show("Please input a username!");
+                return false;
+            }
+
+            //-- SETUP --
+            string userFetchFull = userFetch + user;
+
+            //-- JSON GETTING & PARSING --
+            //Get JSON from pronouns.page API
+            string json = client.DownloadString(userFetchFull);
+
+            //Let JsonConvert do the dirty work of converting to the Profile Struct for us.
+            globalProfile = JsonConvert.DeserializeObject<GlobalProfile>(json);
+
+            //-- FORM TITLE --
+            //Set form title with username (e.g.: A user with the username of "jai_" would get: "jai_ - pronouns.page" for the form title.
+            Text = $"{globalProfile.username} - pronouns.page";
+
+            //-- PROFILE PICTURE --
+            Image profilePic;
+            try
+            {
+                //Download profile picture from data
+                var pfp = client.DownloadData(globalProfile.avatar);
+
+                //Parse byte data to the image and insert into picture box
+                profilePic = ImageUtils.ToImage(pfp);
+            }
+            catch {
+                //Profile Picture doesn't exist. Just keep default.
+                profilePic = null;
+            }
+
+            return FetchUserBase(profilePic);
+        }
+
+        public bool FetchUser(string jsonPath, Image pfp = null)
+        {
+            //Disable save profile button
+            saveProfile.Hide();
+
+            //Let JsonConvert do the dirty work of converting to the Profile Struct for us.
+            try
+            {
+                string json = File.ReadAllText(jsonPath);
+                globalProfile = JsonConvert.DeserializeObject<GlobalProfile>(json);
+            } catch
+            {
+                MessageBox.Show("User data invalid!");
+                return false;
+            }
+
+
+            //-- FORM TITLE --
+            //Set form title with username (e.g.: A user with the username of "jai_" would get: "jai_ - pronouns.page (LOCAL SAVE)" for the form title.
+            Text = $"{globalProfile.username} - pronouns.page (LOCAL SAVE)";
+
+            return FetchUserBase(pfp);
+        }
+
         private void saveProfile_Click(object sender, EventArgs e)
         {
             HomeMenu.SaveProfile(globalProfile, profilePicture.Image);
@@ -362,14 +297,19 @@ namespace Viewer
 
         private void flags_ItemActivate(object sender, EventArgs e)
         {
+            string termLink = "";
             var item = flags.SelectedItems[0];
             if (item.ToolTipText == customFlagTooltip)
             {
-                MessageBox.Show("This is a custom flag! We can't pull more info about it.");
-                return;
+                //MessageBox.Show("This is a custom flag! We can't pull more info about it.");
+                var key = primaryProfile.customFlags.First(x => x.Value == item.Text).Key;
+                termLink = $"{imageCDN}{key}{flagEnd}";
+            }
+            else
+            {
+                termLink = terminologyBase + item.Text;
             }
 
-            var termLink = terminologyBase + item.Text;
             System.Diagnostics.Process.Start(termLink);
         }
 
